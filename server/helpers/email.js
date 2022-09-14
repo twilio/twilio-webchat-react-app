2a8fb8aafb016764d7862094ecd63530beade925
Nayana Dasgupta <ndasgupta@twilio.com>
2022-09-14 16:37:22 +0100

    Initial Email Setup

---
+++ server/helpers/email.js
@@ -0,0 +1,29 @@
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+function createMessage(emailData) {
+    return {
+        to: emailData.recipientAddress,
+        from: process.env.FROM_EMAIL,
+        subject: "Test Email",
+        text: "Test Email"
+    };
+}
+
+async function sendMessage(emailParams) {
+    try {
+      await sgMail.send(createMessage(emailParams));
+      return  { message: `Transcript email sent to: ${emailParams.recipientAddress}`};
+    } catch (error) {
+      console.error(`Error sending transcript email to: ${emailParams.recipientAddress}`, error);
+      if (error.response) {
+        console.error(error.response.body)
+      }
+      return {message};
+    }
+  }
+
+module.exports = {
+    sendMessage
+}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+
 const { validateRequestOriginMiddleware } = require("./middlewares/validateRequestOriginMiddleware");
 const { initWebchatController } = require("./controllers/initWebchatController");
 const { refreshTokenController } = require("./controllers/refreshTokenController");
+const { sendMessage } = require("./helpers/email");
+
 const cors = require("cors");
 const { allowedOrigins } = require("./helpers/getAllowedOrigins");
 
@@ -21,3 +24,13 @@ app.listen(port, () => {
 
 app.post("/initWebchat", validateRequestOriginMiddleware, initWebchatController);
 app.post("/refreshToken", validateRequestOriginMiddleware, refreshTokenController);
+
+app.post("/email", async (req, res) => {
+    try {
+        console.log('receiving data ...');
+        console.log('body is ',req.body);
+        res.json(await sendMessage(req.body));
+    } catch (err) {
+        console.log(err);
+    }
+});
