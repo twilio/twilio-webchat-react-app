ff88f6a9e75daf49d97517f8dd1963505282400c
Nayana Dasgupta <ndasgupta@twilio.com>
2022-09-29 16:13:57 +0100

    Additional generateDuration tests

--- src/utils/generateDuration.test.ts
+++ src/utils/generateDuration.test.ts
@@ -9,4 +9,45 @@ describe("File Preview", () => {
         ];
         expect(generateDuration(transcriptData)).toEqual("2 days 22 hours 53 minutes 50 seconds ");
     });
+
+    it("calculate the duration of the chat lasting 1 minute", () => {
+        const transcriptData: Transcript[] = [
+            { author: "John", body: "hi", timeStamp: new Date("December 17, 2022 03:30:10"), attachedMedia: null },
+            { author: "John", body: "hi", timeStamp: new Date("December 17, 2022 03:31:10"), attachedMedia: null }
+        ];
+        expect(generateDuration(transcriptData)).toEqual("1 minute ");
+    });
+
+    it("calculate the duration of the chat lasting 1 second", () => {
+        const transcriptData: Transcript[] = [
+            { author: "John", body: "hi", timeStamp: new Date("December 17, 2022 04:30:10"), attachedMedia: null },
+            { author: "John", body: "hi", timeStamp: new Date("December 17, 2022 04:30:11"), attachedMedia: null }
+        ];
+        expect(generateDuration(transcriptData)).toEqual("1 second ");
+    });
+
+
+    it("calculate the duration of the chat lasting 1 minute and 2 seconds", () => {
+        const transcriptData: Transcript[] = [
+            { author: "John", body: "hi", timeStamp: new Date("December 17, 2022 04:30:10"), attachedMedia: null },
+            { author: "John", body: "hi", timeStamp: new Date("December 17, 2022 04:31:12"), attachedMedia: null }
+        ];
+        expect(generateDuration(transcriptData)).toEqual("1 minute 2 seconds ");
+    });
+
+    it("calculate the duration of the chat lasting 1 hour", () => {
+        const transcriptData: Transcript[] = [
+            { author: "John", body: "hi", timeStamp: new Date("December 17, 2022 04:30:10"), attachedMedia: null },
+            { author: "John", body: "hi", timeStamp: new Date("December 17, 2022 05:30:10"), attachedMedia: null }
+        ];
+        expect(generateDuration(transcriptData)).toEqual("1 hour ");
+    });
+
+    it("calculate the duration of the chat lasting 1 day and 1 hour", () => {
+        const transcriptData: Transcript[] = [
+            { author: "John", body: "hi", timeStamp: new Date("December 17, 2022 04:30:10"), attachedMedia: null },
+            { author: "John", body: "hi", timeStamp: new Date("December 18, 2022 05:30:10"), attachedMedia: null }
+        ];
+        expect(generateDuration(transcriptData)).toEqual("1 day 1 hour ");
+    });
 });
