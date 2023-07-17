d0bea69daf970a547553cfe5ea0d04c24ca0ea71
mdeshpande <mdeshpande@twilio.com>
2023-07-17 15:22:28 +0530

    adding unit tests for useSanitizer custom hook

---
+++ src/utils/useSanitizer.test.tsx
@@ -0,0 +1,33 @@
+import { act } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
+import { useSanitizer } from "./useSanitizer";
+import validator from "validator";
+describe("useSanitizer custom hook", () => {
+    it("should set the default state on render", () => {
+        const { result } = renderHook(() => useSanitizer());
+
+        expect(result.current.userInput).toBe('');
+    });
+
+    it("should update the local state of the hook after input sanitization", () => {
+        const { result } = renderHook(() => useSanitizer());
+
+        act(() => {
+            result.current.onUserInputSubmit("newText");
+        });
+
+        expect(result.current.userInput).toBe("newText");
+    });
+
+    it("should escape unwanted characters", () => {
+        const blackListSpy = jest.spyOn(validator, "blacklist");
+        const { result } = renderHook(() => useSanitizer());
+
+        act(() => {
+            result.current.onUserInputSubmit("<script>alert('Hello!!!')</script>");
+        });
+
+        expect(result.current.userInput).toBe("scriptalert(Hello!!!)script");
+        expect(blackListSpy).toBeCalled();
+    });
+});
