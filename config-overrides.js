03ddef896047b46a30cd383fdd8a535d1d2e3538
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2023-08-07 13:49:45 +0530

    [FLEXIN-35] - Standardising webpack build for release

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
-// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const webpack = require("webpack");
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
+const filename = "webchat-v3.min.js"
+const cdnDirname = "cdn"
 
 module.exports = function override(config) {
     // ⬇ Output a single file
@@ -8,15 +10,20 @@ module.exports = function override(config) {
     delete config.optimization.splitChunks;
 
     // ⬇ Remove hash from filename
-    config.output.filename = "static/js/[name].min.js";
+    config.output.filename = `static/js/${filename}`;
 
     // ⬇ Prevent (missing) node fallback on path module
     config.resolve.fallback = { ...(config.resolve.fallback || {}), path: false };
 
     config.plugins = [
         ...config.plugins,
-        /* new BundleAnalyzerPlugin(),*/ new webpack.EnvironmentPlugin(["EMAIL_TRANSCRIPT_ENABLED", "DOWNLOAD_TRANSCRIPT_ENABLED"])
+        new FileManagerPlugin({
+              events: {
+                  onEnd: {
+                      copy: [{ source: `build/static/js/${filename}`, destination: `dist/${cdnDirname}/${filename}` }]
+                  }
+              }
+        })
     ];
-
     return config;
 };
