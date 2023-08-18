4d66a645a9b7f85d7a7a5d7a8a50e5fc99a193f0
ashishkumar <ashishkumar@twilio.com>
2023-08-18 12:28:58 +0530

    FLEXIN-74: using each

--- src/utils/regionUtil.test.ts
+++ src/utils/regionUtil.test.ts
@@ -1,14 +1,40 @@
 import { buildRegionalHost } from "./regionUtil";
 
 describe("buildRegionalHost(region)", () => {
-    it("builds correct region for various values", () => {
-        expect(buildRegionalHost("us1")).toBe("");
-        expect(buildRegionalHost("ie1")).toBe(".ie1");
-        expect(buildRegionalHost("")).toBe("");
-        expect(buildRegionalHost("prod")).toBe("");
-        expect(buildRegionalHost("stage-au1")).toBe(".stage-au1");
-        expect(buildRegionalHost("stage-us1")).toBe(".stage");
-        expect(buildRegionalHost("dev-us1")).toBe(".dev");
-        expect(buildRegionalHost("dev-us2")).toBe(".dev-us2");
+    it.each([
+        {
+            regionPassed: "us1",
+            regionExpected: ""
+        },
+        {
+            regionPassed: "ie1",
+            regionExpected: ".ie1"
+        },
+        {
+            regionPassed: "",
+            regionExpected: ""
+        },
+        {
+            regionPassed: "prod",
+            regionExpected: ""
+        },
+        {
+            regionPassed: "stage-au1",
+            regionExpected: ".stage-au1"
+        },
+        {
+            regionPassed: "stage-us1",
+            regionExpected: ".stage"
+        },
+        {
+            regionPassed: "dev-us1",
+            regionExpected: ".dev"
+        },
+        {
+            regionPassed: "dev-us2",
+            regionExpected: ".dev-us2"
+        }
+    ])("builds correct region for various values", ({ regionPassed, regionExpected }) => {
+        expect(buildRegionalHost(regionPassed)).toBe(regionExpected);
     });
 });
