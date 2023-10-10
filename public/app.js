04dbc7814ee85f7bea868338825e437c1cf2ea66
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2023-10-10 13:11:15 +0530

    [FLEXIN-434] - making query more generic for data-theme-pref for app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
   const isLightTheme = urlParams.get("theme") === "light";
-  const el = document.querySelector("[data-theme-pref='light-theme']");
+  const el = document.querySelector("[data-theme-pref]");
 
   el && el.setAttribute("data-theme-pref", isLightTheme ? "light-theme" : "dark-theme");
 
