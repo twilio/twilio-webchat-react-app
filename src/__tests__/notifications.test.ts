626a759bd9202a6401c45b431d38ec252dd11e08
mdeshpande <mdeshpande@twilio.com>
2024-03-26 19:31:39 +0530

    review comments

--- src/__tests__/notifications.test.ts
+++ src/__tests__/notifications.test.ts
@@ -3,7 +3,7 @@ describe("notifications", () => {
     it("should shorten filename if the filename char is too long", () => {
         const longFileName = "Profile-20230724T08574.txt";
         const truncatedName = notifications.shortenFileName(longFileName);
-        expect(truncatedName).toBe("Profile-202307[...]T0857.txt");
+        expect(truncatedName).toBe("Profile-202307[...]08574.txt");
     });
 
     it("should return the filename as is, if the filename charachter length is less than 20 characters", () => {
