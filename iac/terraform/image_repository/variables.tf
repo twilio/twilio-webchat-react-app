5f359b8fd0bfc10a58434fffe4500bcfa3e2af41
Sebastian Murray-Roberts <sebastian.murray@anyvan.com>
2025-09-02 16:42:04 +0200

    Removed unused vars for ECR creation

--- iac/terraform/image_repository/variables.tf
+++ iac/terraform/image_repository/variables.tf
@@ -3,16 +3,6 @@ variable "app_name" {
   description = "Name of the application. This will be used for configuring infrastructure "
 }
 
-variable "env" {
-  type        = string
-  description = "Environment to deploy to"
-}
-
-variable "repository_name" {
-  type        = string
-  description = "Name of the repository. This will be used for configuring infrastructure "
-}
-
 variable "aws_region" {
   type    = string
   default = "eu-west-1"
