9d080bcd882e64695411d2af6c760e302d0ac4c8
Sebastian Murray-Roberts <sebastian.murray@anyvan.com>
2025-09-02 14:53:07 +0200

    Addedd config for client and server repo

--- iac/terraform/image_repository/server_repository.tf
+++ iac/terraform/image_repository/server_repository.tf
@@ -5,7 +5,7 @@ module "ecr_repository" {
   providers = {
     aws = aws.horizontal
   }
-  ecr_repository_name       = var.app_name
+  ecr_repository_name       = "${var.app_name}/twilio-webchat-server"
   allow_cross_account_fetch = true
 
   lifecycle_policy = <<EOF
